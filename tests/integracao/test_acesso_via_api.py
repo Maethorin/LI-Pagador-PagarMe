b3608ea0f1320722b8e681cab2b2d65d91dbac5f
maethorin <maethorin@gmail.com>
2015-03-18 13:47:10 -0300

    Adicionando teste de integração

--- tests/integracao/test_acesso_via_api.py
+++ tests/integracao/test_acesso_via_api.py
@@ -32,3 +32,16 @@ class PagarMeConfiguracaoMeioDePagamentoDaLoja(TestBase):
         json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'configuracao_pagamento': u'PAGAR.ME'}})
         response.status_code.should.be.equal(200)
         configuracao.salvar_formulario.assert_called_with({'token': u'ZES'})
+
+
+class PagarMeEnviandoPagamento(TestBase):
+    url = '/loja/8/meio-pagamento/pagarme/enviar/1234/1'
+
+    @mock.patch('pagador.servicos.GerenciaPedido', mock.MagicMock())
+    @mock.patch('pagador.servicos.GravaEvidencia', mock.MagicMock())
+    @mock.patch('pagador_pagarme.servicos.EntregaPagamento')
+    def test_deve_enviar_pagamento(self, entrega_mock):
+        entrega_mock.return_value = mock.MagicMock(redirect_para=None, resultado={'zas': 'pagamento-enviado'})
+        response = self.app.post(self.url, follow_redirects=True, headers={'authorization': 'chave_aplicacao CHAVE-TESTE'})
+        json.loads(response.data).should.be.equal({u'metadados': {u'api': u'API Pagador', u'resultado': u'sucesso', u'versao': u'1.0'}, u'sucesso': {u'zas': u'pagamento-enviado'}})
+
