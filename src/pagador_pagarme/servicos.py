59407ad3dcc6d77ba56bef9b031acb8cfdd5d5e7
maethorin <maethorin@gmail.com>
2015-06-01 13:42:04 -0300

    Adicionando resultado padrão nas evidências

--- src/pagador_pagarme/servicos.py
+++ src/pagador_pagarme/servicos.py
@@ -97,9 +97,10 @@ class EntregaPagamento(servicos.EntregaPagamento):
             self.situacao_pedido = SituacoesDePagamento.do_tipo(self.resposta.conteudo['status'])
             self.resultado = {'sucesso': self.resposta.conteudo['status'] == 'processing', 'situacao_pedido': self.situacao_pedido, 'alterado_por_notificacao': False}
         elif self.resposta.requisicao_invalida or self.resposta.nao_autorizado:
+            self.situacao_pedido = SituacoesDePagamento.do_tipo('refused')
             titulo = u'A autenticação da loja com o PAGAR.ME falhou. Por favor, entre em contato com nosso SAC.' if self.resposta.nao_autorizado else u'Ocorreu um erro nos dados enviados ao PAGAR.ME. Por favor, entre em contato com nosso SAC.'
             if not self._verifica_erro_em_conteudo(titulo):
-                self.resultado = {'sucesso': False, 'mensagem': u'nao_aprovado', 'fatal': True}
+                self.resultado = {'sucesso': False, 'mensagem': u'nao_aprovado', 'situacao_pedido': self.situacao_pedido, 'fatal': True}
         else:
             self._verifica_erro_em_conteudo(u'Não foi obtida uma resposta válida do PAGAR.ME. Nosso equipe técnica está avaliando o problema.')
 
