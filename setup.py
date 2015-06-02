2b9169d1bf3f77c247fbe574613057cf59174ac8
maethorin <maethorin@gmail.com>
2015-06-02 13:26:21 -0300

    Filtrando por pagador que exige https #35681426970296

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-pagarme',
-    version='0.0.22',
+    version='0.0.23',
     url='https://github.com/lojaintegrada/LI-Pagador-PagarMe',
     license='MIT',
     description='Meio de pagamento usando o PAGAR-ME (https://pagar.me/)',
